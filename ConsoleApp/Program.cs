79d2216747a4e68d9e58ef538f06f146b462bbac
Tanatorn <Tanatorn@DESKTOP-6LC555V>
2020-04-16 15:59:00 +0700

    edit View Consolelog

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -5,12 +5,12 @@ namespace ConsoleApp
 {
     internal class Program
     {
-        private static SkateboardContext context = new SkateboardContext();
+        //private static SkateboardContext context = new SkateboardContext();
 
         private static void Main(string[] args)
         {
-            context.Database.EnsureCreated();
-            Console.ReadKey();
+            //context.Database.EnsureCreated();
+            //Console.ReadKey();
         }
     }
 }
